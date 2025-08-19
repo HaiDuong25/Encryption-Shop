7f9bbbd3869fe75c72235b7857f7b0c69046a744
luongbtph52665 <luongbtph52665@gmail.com>
2025-08-19 21:51:53 +0700

    sửa

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -27,9 +27,7 @@
 
             <div class="mb-3">
                 <label class="fw-semibold mb-1"><strong>Mô tả sản phẩm:</strong></label>
-                <div class="fw-bold text-secondary" style="margin-top: 0 !important; padding-top: 0 !important; word-break: break-word; white-space: pre-wrap;">
-                    {!! nl2br(e(preg_replace('/^\s*(\r?\n)+/', '', $product->description))) !!}
-                </div>
+                <div class="fw-bold text-secondary" style="margin-top: 0 !important; padding-top: 0 !important; word-break: break-word; white-space: pre-wrap;">{!! nl2br(e(preg_replace('/^\s*(\r?\n)+/', '', $product->description))) !!}</div>
             </div>
 
             <hr>
